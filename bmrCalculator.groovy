061cfed04d0afa41a077fd14ef0c3b0d3e1d30fa
peterkoter <peterkoter@users.noreply.github.com>
2015-11-18 12:36:35 +0200

    Update bmrCalculator.groovy

--- bmrCalculator.groovy
+++ bmrCalculator.groovy
@@ -1,7 +1,8 @@
-import static Activity.NONE
+import static Activity.*
+import static Gender.*
 
 enum Gender {
-    MEN, WOMEN
+    MALE, FEMALE
 }
 
 enum Activity {
@@ -11,7 +12,7 @@ enum Activity {
 /**
  * This is a Basal Metabolic Rate Calculator based on Harris Benedict's formula
  *
- * @param gender Men or Women
+ * @param gender Male or Female
  * @param weight in kg
  * @param height in cm
  * @param age in years
@@ -25,13 +26,13 @@ def basalMetabolicRate(Gender gender, Number weight, Number height, Number age) 
     def ageScale
 
     switch (gender) {
-        case Gender.MEN:
+        case MALE:
             offset = 88.362
             weightScale = 13.397
             heightScale = 4.799
             ageScale = 5.677
             break
-        case Gender.WOMEN:
+        case FEMALE:
             offset = 447.593
             weightScale = 9.247
             heightScale = 3.098
@@ -43,27 +44,14 @@ def basalMetabolicRate(Gender gender, Number weight, Number height, Number age) 
 
 def dailyCalorieIntake(Activity activity, Number basalMetabolicRate) {
     switch (activity) {
-        case NONE:                              // Little to no exercise
-            return basalMetabolicRate * 1.2
-        case Activity.LIGHT:                    // Light exercise (1–3 days per week)
-            return basalMetabolicRate * 1.375
-        case Activity.MODERATE:                 // Moderate exercise (3–5 days per week)
-            return basalMetabolicRate * 1.55
-        case Activity.HEAVY:                    // Heavy exercise (6–7 days per week)
-            return basalMetabolicRate * 1.725
-        case Activity.VERY_HEAVY:               // Very heavy exercise (twice per day, extra heavy workouts)
-            return basalMetabolicRate * 1.9
+        case NONE: return basalMetabolicRate * 1.2          // Little to no exercise
+        case LIGHT: return basalMetabolicRate * 1.375       // Light exercise (1–3 days per week)
+        case MODERATE: return basalMetabolicRate * 1.55     // Moderate exercise (3–5 days per week)
+        case HEAVY: return basalMetabolicRate * 1.725       // Heavy exercise (6–7 days per week)
+        case VERY_HEAVY: return basalMetabolicRate * 1.9    // Very heavy exercise (twice per day, extra heavy workouts)
     }
 }
 
-/**
- * This is a Body Mass Index Calculator
- *
- * @param weight in kg
- * @param height in meter
- * @return the persons Body Mass Index
- */
-
-def somebodysBmr = basalMetabolicRate(Gender.MEN, 100, 170, 27)
-def recommendedDailyCalorieIntake = dailyCalorieIntake(Activity.VERY_HEAVY, somebodysBmr)
+def somebodysBmr = basalMetabolicRate(FEMALE, 100, 170, 27)
+def recommendedDailyCalorieIntake = dailyCalorieIntake(VERY_HEAVY, somebodysBmr)
 println "Your BMR: ${somebodysBmr}, recommended Daily Calorie Intake: ${recommendedDailyCalorieIntake}"
